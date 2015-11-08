5d4cabd50d4e3df52b8e8dd580dba4fae5cfc85b
vamsimokari <vamsimokari@gmail.com>
2015-11-08 18:28:46 +0530

    Update test1.erl

--- test1.erl
+++ test1.erl
@@ -2,6 +2,10 @@
 %-compile(export_all).
 -export([start/1]).
 -export([loop/1]).
+%start(Node)=Pid()
+%Node=atom(), The node where print-process shall be started,
+%%starts a print process and returns the process id of newly
+%% created process.
 start(Node)->
         spawn(Node,test1,loop,[]).
 loop()->
